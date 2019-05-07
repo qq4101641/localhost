a1e3d2d7861159dba836a07ab18e4d2e78dc5166
root <4101641@qq.com>
2019-05-07 18:20:00 +0800

    提交

---
+++ day47/src/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git proje999988899999999988899999999999ct");
+        System.out.println("heelo git project");
+        System.out.println("heelo git 999999999999999");
+    }
+}
